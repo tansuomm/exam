e74c76127e2c4de402d434eab7455f95a9582043
yuanbao <1309877947@qq.com>
2016-05-06 10:22:59 +0800

    x

--- src/org/yohta/filter/SessionFilter.java
+++ src/org/yohta/filter/SessionFilter.java
@@ -1,27 +1,22 @@
 package org.yohta.filter;
 
 import java.io.IOException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet Filter implementation class SessionFilter
  */
-@WebFilter("/SessionFilter")
 public class SessionFilter implements Filter {
-
-    /**
-     * Default constructor. 
-     */
-    public SessionFilter() {
-        // TODO Auto-generated constructor stub
-    }
-
+	FilterConfig fConfig;
 	/**
 	 * @see Filter#destroy()
 	 */
@@ -33,18 +28,41 @@ public class SessionFilter implements Filter {
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		// place your code here
-
-		// pass the request along the filter chain
-		chain.doFilter(request, response);
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		HttpSession session = req.getSession();
+		String nologinPaths = fConfig.getInitParameter("noLoginPaths");
+		//req.setCharacterEncoding("UTF-8");
+		String charset = fConfig.getInitParameter("charset");
+		if(charset==null){
+			charset="utf-8";
+		}
+		req.setCharacterEncoding(charset);
+		
+		if(nologinPaths!=null){
+			String[] strArr = nologinPaths.split(";");
+			for(int i = 0;i<strArr.length;i++){
+				if(strArr[i]==null||"".equals(strArr[i])){
+					continue;
+				}
+				if(req.getRequestURI().indexOf(strArr[i])!=-1){
+					chain.doFilter(request, response);
+					return;
+				}
+			}
+		}
+		if(session.getAttribute("username")!=null){
+			chain.doFilter(request, response);
+		}else{
+			res.sendRedirect("/login/loginPre");
+		}
 	}
 
 	/**
 	 * @see Filter#init(FilterConfig)
 	 */
 	public void init(FilterConfig fConfig) throws ServletException {
-		// TODO Auto-generated method stub
+		this.fConfig=fConfig;
 	}
 
 }
